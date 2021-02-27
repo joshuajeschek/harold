61ec769b8f9ae2479015c961b52f54b820220946
vanitasboi <joshua.jeschek@gmail.com>
2021-02-27 16:54:35 +0100

    capitalize first letter function

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -1,35 +1,44 @@
 const { unlink } = require('fs');
 const config = require('../../');
 
-module.exports = {
-    deleteFile: async function(path) {
-        unlink(path, (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    },
-    compileMongoUrl: function() {
-        const app = process.argv[2];
-        if (!app) {
-            console.log(`Invalid app provided. [${process.argv[2]}`);
-            exit(1);
-        }
-        let url = config.discord.mongourl;
-        url = url.replace('<name>', process.env.MONGO_NAME);
-        url = url.replace('<password>', process.env.MONGO_PASSWORD);
-        switch (app) {
-            case 'T':
-                url = url.replace('<app>', 'chester'); 
-                return [ url, 'chester' ];
-        
-            case 'H':
-                url = url.replace('<app>', 'harold'); 
-                return [ url, 'harold' ];
+const capitalizeFirstLetter = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-            default:
-                // should never occur
-                break;
+const deleteFile = async function (path) {
+    unlink(path, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+const compileMongoUrl = function() {
+    const app = process.argv[2];
+    if (!app) {
+        console.log(`Invalid app provided. [${process.argv[2]}`);
+        exit(1);
     }
+    let url = config.discord.mongourl;
+    url = url.replace('<name>', process.env.MONGO_NAME);
+    url = url.replace('<password>', process.env.MONGO_PASSWORD);
+    switch (app) {
+        case 'T':
+            url = url.replace('<app>', 'chester'); 
+            return [ url, 'chester' ];
+    
+        case 'H':
+            url = url.replace('<app>', 'harold'); 
+            return [ url, 'harold' ];
+
+        default:
+            // should never occur
+            break;
     }
+}
+
+module.exports = {
+    capitalizeFirstLetter,
+    deleteFile,
+    compileMongoUrl    
 }
