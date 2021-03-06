e7f08ba3f4577e779c2fc7f59630bc5546e777cf
vanitasboi <joshua.jeschek@gmail.com>
2021-03-07 00:57:11 +0100

    refactored the harold client. does it make sense?

--- src/main.js
+++ src/main.js
@@ -1,38 +1,21 @@
-const Commando = require('discord.js-commando');
+const HaroldClient = require('./modules/harold_client');
+const { exit } = require('process');
+require('dotenv').config();
 
-const { commandoSetup, logIn } = require ('./modules/dc_startup.js');
+// instantiate a new harold client (discord + steam)
+const harold = new HaroldClient();
 
-const config = require('../config.json');
-
-const { FriendlyError } = require('discord.js-commando');
-
-
-const dc_client = new Commando.Client({
-    owner: config.discord.owner,
-    commandPrefix: config.discord.prefix,
-});
-
-/* Logs if bot is ready */
-dc_client.on('ready', () => {
-    console.log(`✅ Logged in as ${dc_client.user.tag}!`);
-    dc_client.user.setActivity('Counter-Strike: Global Offensive', {
-        type: 'PLAYING',
-    });
-});
-
-/* Logging of DM Messages */
-dc_client.on('message', (msg) => {
-    if (msg.channel.type === 'dm' && msg.author != dc_client.user) {
-        console.log(`>>> [DM] ${msg.author.tag}: ${msg.content}`);
-    }
-});
-
-
-dc_client.on('commandError', (cmd, err) => {
-    if (err instanceof FriendlyError) return;
-    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-});
-
-/** Setup and Login of discord client */
-commandoSetup(dc_client);
-logIn(dc_client);
+// log them in
+if (process.argv.length < 2) {
+    console.log('Please specify an application [H/T]');
+    exit(1);
+} else if (process.argv[2] == 'H') {
+    console.log('Logging in as BOT Harold');
+    harold.connect(process.env.HAROLD_TOKEN);
+} else if (process.argv[2] == 'T') {
+    console.log('Logging in as Chester McTester');
+    harold.connect(process.env.TESTBOT_TOKEN);
+} else {
+    console.log(`Invalid app provided. [${process.argv[2]}`);
+    exit(1);
+}
---
+++ src/modules/harold_client.js
@@ -0,0 +1,80 @@
+'use strict'
+const Commando = require('discord.js-commando');
+const config = require('../../config.json');
+const SteamInteraction = require('./steam/steam_interaction');
+const { FriendlyError } = require('discord.js-commando');
+const { compileMongoUrl } = require('./mongo');
+const path = require('path');
+const { MongoClient } = require('mongodb');
+const { MongoDBProvider } = require('commando-provider-mongo')
+
+
+class HaroldClient {
+    constructor() {
+        this.dc_client = new Commando.Client({
+            owner: config.discord.owner,
+            commandPrefix: config.discord.prefix,
+        });
+
+        this.steam_client = new SteamInteraction();
+    }
+
+    connect (token) {
+        this.steam_client.connect();
+        this.discordEvents();
+        this.commandoSetup();
+        this.dc_client.login(token);
+    }
+
+    discordEvents () {
+        /* Logs if bot is ready */
+        this.dc_client.on('ready', () => {
+            console.log(`💬 Logged in as ${this.dc_client.user.tag}!`);
+            this.dc_client.user.setActivity('Counter-Strike: Global Offensive', {
+                type: 'PLAYING',
+            });
+        });
+
+        /* Logging of DM Messages */
+        this.dc_client.on('message', (msg) => {
+            if (msg.channel.type === 'dm' && msg.author != this.dc_client.user) {
+                console.log(`>>> [DM] ${msg.author.tag}: ${msg.content}`);
+            }
+        });
+
+        this.dc_client.on('commandError', (cmd, err) => {
+            if (err instanceof FriendlyError) return;
+            console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+        });
+    }
+
+    commandoSetup () {
+        this.dc_client.registry
+            // Registers the custom command groups
+            .registerGroups([
+                ['util', 'Utility'],
+                ['vote', 'Voting'],
+            ])
+            // Registers select default commands
+            .registerDefaultTypes()
+            .registerDefaultGroups()
+            .registerDefaultCommands({
+                ping: false,
+                unknownCommand: false
+            })
+            // Registers all of the commands in the ./commands/ directory
+            .registerCommandsIn(path.join(__dirname, '../commands'));
+        console.log('Loaded these commands:');
+        console.log(this.dc_client.registry.commands.keys());
+        const [ mongo_url, db_name ] = compileMongoUrl()
+        this.dc_client
+            .setProvider(
+                MongoClient.connect(mongo_url, { useUnifiedTopology: true }).then(
+                    (client) => new MongoDBProvider(client, db_name)
+                )
+            )
+            .catch(console.error);
+    }
+}
+
+module.exports = HaroldClient;
